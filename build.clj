8896bd37ab8d5e959402271fce03fa78f8a724f0
sam <sammy>
2026-01-08 14:14:46 -0500

    deploy to clojars

---
+++ build.clj
@@ -0,0 +1,25 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+
+(def lib 'sstraust/deft)
+(def version "0.1.0") ;; or read from file, etc
+(def class-dir "target/classes")
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+;; delay to defer side effects (artifact downloads)
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis @basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
