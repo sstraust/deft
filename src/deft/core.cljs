45be000f78fc14aafb9fa5784453f679cae8f8f9
sam <sammy>
2026-02-23 11:29:40 -0500

    bugfix for protocol keywords

--- src/deft/core.cljs
+++ src/deft/core.cljs
@@ -2,5 +2,5 @@
   (:require [deft.core-shared :as core-shared])
   (:require-macros [deft.core]))
 
-(def get-deft-mutable-registry deft.core-shared/get-deft-mutable-registry)
-(def use-deft-malli-registry! deft.core-shared/use-deft-malli-registry!)
+(def get-deft-mutable-registry deft.core-shared/get-deft-mutable-registry-internal)
+(def use-deft-malli-registry! deft.core-shared/use-deft-malli-registry-internal!!)
