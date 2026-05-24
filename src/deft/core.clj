bebd7bf987e0a301b73f9a36a9b2d72c4058e1d1
sam <sam@mac.myfiosgateway.com>
2026-05-24 16:26:45 -0400

    protocol keywords bugfix

--- src/deft/core.clj
+++ src/deft/core.clj
@@ -182,7 +182,7 @@
                                 :else (into []
                                              (concat (:allows-external (::opts interface-impls))
                                                      (map (fn [impl] (get-method-impl-name interface-name impl &env)) (::impls interface-impls))))))
-                (derive ~type-name ~(keyword (name (str *ns*)) (name interface-name)))))))
+                (derive ~type-name ~(::name interface-name))))))
 
 ;; TODO!! Check what the print methods should be for cljs
 (defmacro define-record-like-print-methods [type-name]
